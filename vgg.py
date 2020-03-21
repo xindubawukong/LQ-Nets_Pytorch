c384e277c23659b81138973bcbec9c28c485f998
dxy <ding-xy16@mails.tsinghua.edu.cn>
2020-03-21 17:20:53 +0800

    ff

--- vgg.py
+++ vgg.py
@@ -23,7 +23,7 @@ model_urls = {
 
 class VGG(nn.Module):
 
-    def __init__(self, features, num_classes=1000, init_weights=True):
+    def __init__(self, features, num_classes=1000, init_weights=True, **kwargs):
         super(VGG, self).__init__()
         self.features = features
         self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
